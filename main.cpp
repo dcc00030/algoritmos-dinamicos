30e61284e5333ff85c8c7d868f3aab087cb5604c
L1pc <L1pc@L1pc04>
2016-04-21 11:57:33 +0200

    ejercicio 14 finalizado

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
  *
  * Created on 21 de abril de 2016, 10:48
  */
-
+#include <iostream>
 #include <cstdlib>
 
 using namespace std;
@@ -18,8 +18,23 @@ using namespace std;
 /*
  * 
  */
+void pd(int v[], int n){
+    v[0]=3;
+    v[1]=3;
+    for(int i = 2; i < n; i++){
+        if((i%2)== 0){
+            v[i]=v[i-2]+3;
+        }else{
+            v[i]=v[i-1]-3;
+        }
+    }
+}
 int main(int argc, char** argv) {
-
+    //ejercicio14
+    int v[100];
+    int n = 10;
+    pd(v,n);
+    cout<<v[n-1]<<endl;
     return 0;
 }
 
